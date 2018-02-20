b03a4a1cf3198f7c484da997245beb7c9208accd
Maor Blaustein <36456065+BluesNote1@users.noreply.github.com>
2018-02-20 23:09:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,20 @@ if [ "$(id -u)" -ne "0" ]; then
     exit 1
 fi
 
-for dep in 'apt-get install fdupes libimage-exiftool-perl python3 python3-pip python-qt4', 'pip3 install requests'; do
+for dep in 'apt-get update' 'apt-get -y install fdupes libimage-exiftool-perl python3 python3-pip python-qt4' 'pip3 install requests'; do
     $dep
     if [ "$?" -ne "0" ]; then
-        echo ' [#] Failed installing metax dependencies, try manually'  
+        echo ' [-] Failed installing metax dependencies, try manually'  
         exit 1
     fi
 done
 
 chmod +x gui.py
-ln -s -T ./gui.py /usr/local/bin/metax
+ln -s -T "$(pwd)/gui.py" /usr/local/bin/metax
 if [ "$?" -ne "0" ]; then
-    echo ' [#] Failed creating link of metax in /usr/local/bin/, try manually'  
+    echo ' [-] Failed creating link of metax in /usr/local/bin/, try manually'  
     exit 1
 fi
 
+echo ' [+] Dependencies installed and program setup correctly, you can run "metax" in a terminal'
 exit 0
